f31f255fb7243b61721eee044e743c4608d3a723
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-02-17 13:07:12 -0800

    background color for header

---
+++ app/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="bg-gradient-to-r from-purple-700 via-yellow-700 to-yellow-300">
+      <div>Header</div>
+    </div>
+  );
+}
+
+export default Header;
