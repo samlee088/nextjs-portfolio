b3ba72f16ece120e84b53b195c8f1340580e79cf
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-02-17 13:32:21 -0800

    header light/dark mode functionality

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { ThemeToggler } from "./ThemeToggler";
 
 function Header() {
   return (
-    <div className="bg-gradient-to-r from-purple-700 via-yellow-700 to-yellow-300">
-      <div>Header</div>
+    <div className=" fixed w-full z-20 top-0 flex items-center justify-between p-5 bg-gradient-to-r from-purple-700 via-yellow-700 to-yellow-300">
+      <div>Samuel Lee</div>
+
+      <div className="flex space-x-2">
+        <ThemeToggler />
+      </div>
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import { ThemeProvider } from "./components/ThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Header/>
-        {children}</body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
