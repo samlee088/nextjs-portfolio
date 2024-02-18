736a93df0a75e0199686f1c811dc67fa6366b1c0
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-02-17 20:05:17 -0800

    resumeskills page update with the centering of skill and icon update with header name text color

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import { ThemeToggler } from "./ThemeToggler";
 function Header() {
   return (
     <div className=" fixed w-full z-20 top-0 flex items-center justify-between p-5 bg-gradient-to-r from-purple-700 via-yellow-700 to-yellow-300">
-      <div>Samuel Lee</div>
+      <div className="text-white">Samuel Lee</div>
 
       <div className="flex space-x-2">
         <ThemeToggler />
