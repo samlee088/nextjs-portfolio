72041511115b38739febbf7125118ab1d6e42611
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-02-20 20:20:01 -0800

    Introduction: -added introduction text -applied gradient css styling to introduction text -created an introduction text component

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import fullSeattle from "./images/Seattle_skyline_from_Kerry_Park,_Sept._2015_(21418893286).jpg";
 import Image from "next/image";
+import IntroductionText from "./IntroductionText";
 
 function Introduction() {
   return (
     <div>
-      <Image
-        src={fullSeattle}
-        alt="landscape picture of Seattle"
-      />
+      <Image src={fullSeattle} alt="landscape picture of Seattle" />
+
+      <IntroductionText />
     </div>
   );
 }
---
+++ components/IntroductionText.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function IntroductionText() {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="gradient-text-micro-5-regular text-9xl ">Software Developer</h1>
+      
+    </div>
+  );
+}
+
+export default IntroductionText;
