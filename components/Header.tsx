92eeb6a3674c28c85bf16e42cee34ac5e5a005dd
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-02-29 16:48:13 -0800

    animation refactoring -added in react animation package. The first step was to make an adjustment to the header and have the contents slide across the page to the right with an an opacity adjustment

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,30 @@
+"use client";
 import React from "react";
 import { ThemeToggler } from "./ThemeToggler";
+import { motion } from "framer-motion";
 
 function Header() {
   return (
-    <div className=" fixed w-full z-20 top-0 flex items-center justify-between p-5 bg-gradient-to-r from-purple-700 via-yellow-700 to-yellow-300">
-      <div className="text-white">Samuel Lee</div>
+    <div className=" fixed w-full z-20 top-0  p-5 bg-gradient-to-r from-purple-700 via-yellow-700 to-yellow-300">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        className="  flex items-center justify-between "
+      >
+        <div className="text-white">Samuel Lee</div>
 
-      <div className="flex space-x-2">
-        <ThemeToggler />
-      </div>
+        <div className="flex space-x-2">
+          <ThemeToggler />
+        </div>
+      </motion.div>
     </div>
   );
 }
